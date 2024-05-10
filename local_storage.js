3495e9af2f2795cd302b773c3fe5be4c528efc75
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-05-10 09:32:03 +0200

    Update local_storage.js

--- local_storage.js
+++ local_storage.js
@@ -1,6 +1,10 @@
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark');
+        document.getElementById("svg-light").style.display = "none";
+        document.getElementById("svg-dark").style.display = "inline";
     } else {
         document.documentElement.classList.remove('dark')
+         document.getElementById("svg-dark").style.display = "none";
+        document.getElementById("svg-light").style.display = "inline";
     }
 
